8be5c69bdc88e341b976c7108838d0780896ddf4
Luiza <luizaschettinii@gmail.com>
2025-04-03 09:01:26 -0300

    refactor: colors footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,12 @@
 import styles from "../styles/Footer.module.css";
-import FooterLogo from "./FooterLogo";
-import FooterLinksCategories from "./FooterLinksCategories";
-import FooterSocial from "./FooterSocial";
+import FooterSection from "./FooterSection";
 import FooterBottom from "./FooterBottom";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <FooterLogo />
-        <FooterLinksCategories />
-        <FooterSocial />
+      <FooterSection />
       </div>
       <FooterBottom />
     </footer>
